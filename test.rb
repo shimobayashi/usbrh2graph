715ef6639b8318c3cb0c1ae3e7e190f8e47915f5
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-08-30 12:42:06 +0900

    initial commit

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+require_relative 'simple-googlespreadsheet-ruby/spreadsheet'
+
+account = YAML.load_file('account.yaml')
+
+spreadsheet_key = "0AlcQ3JEpuSzqdFNSVVFTcWlDSXI1dXZzdnBkQ0stLUE"
+
+session = GoogleSpreadsheet.login(account['email'], account['password'])
+
+ws = session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
+
+puts ws.title
+
+p ws.row_count
+ws << [Time.now.sprintf('%Z'), 30, 60]
+p ws.row_count
