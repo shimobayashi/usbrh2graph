20c663f8b1ccc3dd9f61f29c73ef9191864a7553
shimobayashi <akimasa@shimobayashi.name>
2012-11-17 13:10:40 +0900

    nice catch

--- logger.rb
+++ logger.rb
@@ -7,25 +7,34 @@ spreadsheet_key = "0AlcQ3JEpuSzqdFNSVVFTcWlDSXI1dXZzdnBkQ0stLUE"
 session = GoogleSpreadsheet.login(account['email'], account['password'])
 ws = session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
 
-devices = RHDevices.new
-devices.each do |device|
-  puts "name                 : #{device.name}"
-  puts "version              : #{device.version}"
+begin
+  devices = RHDevices.new
+  device = devices.divices[0]
+  if device
+    puts "name                 : #{device.name}"
+    puts "version              : #{device.version}"
 
-  while true
-    begin
-      now = Time.now.strftime('%F %T')
-      t,h = device.getTempHumidTrue
-      redo unless t and h
-      puts "now                  : #{now}"
-      puts "corrected temperature: #{t}"
-      puts "corrected humidity   : #{h}"
-      ws << [now, h, t]
-      sleep 60 * 60
-    rescue Exception
-      puts "Exception: #{$!.message}"
-      puts $1.backtrace
-      retry
+    while true
+      begin
+        now = Time.now.strftime('%F %T')
+        t,h = device.getTempHumidTrue
+        redo unless t and h
+        puts "now                  : #{now}"
+        puts "corrected temperature: #{t}"
+        puts "corrected humidity   : #{h}"
+        ws << [now, h, t]
+        sleep 60 * 60
+      rescue Exception
+        puts "Exception: #{$!.message}"
+        puts $1.backtrace
+        retry
+      end
     end
   end
+rescue => exc
+  puts "Exception: #{$!.message}"
+  puts $1.backtrace
+  puts 'wait 60sec...'
+  sleep 60
+  retry
 end
