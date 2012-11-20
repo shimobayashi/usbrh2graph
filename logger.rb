ef08039a1788fff7b9f1b4f1d9f883cf07517c28
shimobayashi <akimasa@shimobayashi.name>
2012-11-20 23:25:54 +0900

    fix

--- logger.rb
+++ logger.rb
@@ -2,12 +2,12 @@ require 'yaml'
 require_relative 'simple-googlespreadsheet-ruby/spreadsheet'
 require_relative 'USBMeter'
 
-account = YAML.load_file('account.yaml')
-spreadsheet_key = "0AlcQ3JEpuSzqdFNSVVFTcWlDSXI1dXZzdnBkQ0stLUE"
-session = GoogleSpreadsheet.login(account['email'], account['password'])
-ws = session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
-
 begin
+  account = YAML.load_file('account.yaml')
+  spreadsheet_key = "0AlcQ3JEpuSzqdFNSVVFTcWlDSXI1dXZzdnBkQ0stLUE"
+  session = GoogleSpreadsheet.login(account['email'], account['password'])
+  ws = session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
+
   devices = RHDevices.new
   device = nil
   devices.each {|d| device = d}
